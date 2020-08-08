fe996525b787566682d739881c40557925c5f81a
Solomonz <solomonzitter@gmail.com>
2020-08-08 15:49:58 +0300

    updates to server logic, now handling updates for ties or reveals in a smarter way. changed to using notion of "unseen state" to represent pending updates that have not yet been retrieved by room members. Added a new room state in between tie and revealed.

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ api = Api(app)
 rooms = {}
 lock = threading.Lock()
 
-ROOM_STATES = {'open': 0, 'closed': 1, 'tie': 2, 'revealed': 3}
+ROOM_STATES = {'open': 0, 'closed': 1, 'tie': 2, 'locked': 3, 'revealed': 4}
 for s, e in list(ROOM_STATES.items()):
     ROOM_STATES[e] = s
 
@@ -36,6 +36,7 @@ class RoomVIP(Resource):
             rooms[code] = {
                 "state": ROOM_STATES['open'],
                 "votes": {},
+                "unseen_state": [],
             }
 
             return code, 201
@@ -54,7 +55,7 @@ class RoomVIP(Resource):
     def delete(self, room_code):
         with lock:
             global rooms
-            if room_code in rooms and rooms[room_code]['state'] not in {ROOM_STATES['tie'], ROOM_STATES['revealed']}:
+            if room_code in rooms and rooms[room_code]['state'] not in {ROOM_STATES['locked'], ROOM_STATES['revealed']}:
                 del rooms[room_code]
             return '', 200
     
@@ -88,18 +89,16 @@ class RoomAttendance(Resource):
         with lock:
             global rooms
             room = rooms.get(room_code, None)
-            if room is not None and name in room['votes'] and room['state'] not in {ROOM_STATES['tie'], ROOM_STATES['revealed']}:
+            if room is not None and name in room['votes'] and room['state'] not in {ROOM_STATES['locked'], ROOM_STATES['revealed']}:
                 del room['votes'][name]
             return '', 200
 
 
 class RoomVoting(Resource):
-    def post(self, room_code):
+    def post(self, room_code, name):
         parser = reqparse.RequestParser()
-        parser.add_argument("name", type=str, required=True)
         parser.add_argument("selection", type=float, required=True)
         params = parser.parse_args()
-        name = params.name
         selection = params.selection
         with lock:
             global rooms
@@ -113,34 +112,41 @@ class RoomVoting(Resource):
 
             # This isn't really the best validation I could do, but most of the validation happens clientside anyways
             # so I'm not too cut up about it
-            if not ((1 <= selection <= 10) if (room['state'] != ROOM_STATES['tie']) else (0.75 <= selection <= 10.25)):
+            if not ((1 <= selection <= 10) if (room['state'] != ROOM_STATES['locked']) else (0.75 <= selection <= 10.25)):
                 return 'Invalid Selection', 403
             room['votes'][name] = selection
             return '', 201
 
-    def get(self, room_code):
+    def get(self, room_code, name):
         with lock:
             global rooms
             room = rooms.get(room_code, None)
             if room is None:
                 return 'Unknown Room Code', 404
+
+            if name not in room['votes']:
+                return 'Unknown Name', 403
             
             if room['state'] == ROOM_STATES['revealed']:
-                room['remaining_unseen'] -= 1
-                if room['remaining_unseen'] == 0:
+                if name not in room['unseen_state']:
+                    return 'Unknown Name', 403
+                room['unseen_state'].remove(name)
+                if len(room['unseen_state']) == 0:
                     room = cp(room)
                     del rooms[room_code]
+            elif room['state'] == ROOM_STATES['locked']:
+                if name not in room['unseen_state']:
+                    return dumps(room), 200
+                else:
+                    room['unseen_state'].remove(name)
+                    return dumps({'state': ROOM_STATES['tie'], 'votes': room['votes']})
             return dumps(room), 200
     
-    def delete(self, room_code):
-        parser = reqparse.RequestParser()
-        parser.add_argument("name", type=str, required=True)
-        params = parser.parse_args()
-        name = params.name
+    def delete(self, room_code, name):
         with lock:
             global rooms
             room = rooms.get(room_code, None)
-            if room is not None and room['state'] not in {ROOM_STATES['tie'], ROOM_STATES['revealed']}:
+            if room is not None:
                 room['votes'][name] = None
             return '', 200
 
@@ -155,13 +161,16 @@ class RoomReveal(Resource):
             if any(filter(lambda t: t[1] is None, room['votes'].items())):
                 return 'Not everyone has voted', 403
             highest = max(room['votes'].values())
-            if len(list(filter(lambda t: t[1] == highest, room['votes'].items()))) > 1:
-                room['state'] = ROOM_STATES['tie']
+            winners = list(map(lambda t: t[0], filter(lambda t: t[1] == highest, room['votes'].items())))
+            if len(winners) > 1:
+                room['state'] = ROOM_STATES['locked']
+                room['unseen_state'] = list(room['votes'].keys())
                 for name in room['votes'].keys():
                     room['votes'][name] = None
             else:
                 room['state'] = ROOM_STATES['revealed']
-                room['remaining_unseen'] = len(room['votes'])
+                room['unseen_state'] = list(room['votes'].keys())
+                room['winner'] = winners[0]
 
             return '', 201
 
@@ -175,7 +184,7 @@ class RoomDebug(Resource):
 
 api.add_resource(RoomVIP, "/room", "/room/", "/room/<string:room_code>", "/room/<string:room_code>/")
 api.add_resource(RoomAttendance, "/room/<string:room_code>/attend", "/room/<string:room_code>/attend/")
-api.add_resource(RoomVoting, "/room/<string:room_code>/vote", "/room/<string:room_code>/vote/")
+api.add_resource(RoomVoting, "/room/<string:room_code>/vote/<string:name>", "/room/<string:room_code>/vote/<string:name>/")
 api.add_resource(RoomReveal, "/room/<string:room_code>/reveal", "/room/<string:room_code>/reveal/")
 api.add_resource(RoomDebug, "/room/debug/", "/room/debug/")
 
